9c0010331d2a3bbfebff21c157d07622d73d75b2
Matias Chavez <mcha7674@outlook.com>
2023-02-25 23:05:25 -0600

    update

--- OrbitalMechanics/main.py
+++ OrbitalMechanics/main.py
@@ -2,123 +2,53 @@ import numpy as np
 from matplotlib import pyplot as plt
 import orbit
 import render
-import pygame
-import os.path
-from pathlib import Path
 
+############### Sub Routines ##################
 def VelYforCircOrbit(distFromSun):
     return 2 * np.pi * np.sqrt(orbit.Orbit.M / distFromSun)
 def VelYGeneral(e, distFromSun, planetMass):
     return np.sqrt(orbit.Orbit.G*orbit.Orbit.M) * np.sqrt((1-e)/(distFromSun*(1+e))*(1+planetMass/orbit.Orbit.M))
-def plotOrbit():
-    # Initiating Variables
-    duration = 20.0 # years
-    dt = 0.0001
-    e = 0.206
-    x0 = 0.39*(1+e)
-    # Initiate Orbiting planet
-    earth = orbit.Body(0.001, 3.00273e-6)
-    startPos = (x0, 0) # In AU
-    v0y_Circ = VelYforCircOrbit(np.sqrt(startPos[0]**2 + startPos[1]**2))
-    v0y = VelYGeneral(e, x0, earth.mass)
-    startVel = (0, v0y_Circ) # AU/yr
-    B = 2.5
-    # Initiate Orbit
-    myOrbit1 = orbit.Orbit(earth, startPos, startVel, dt, B)
-    # Initiate Data Collection Variables
-    x = []
-    y = []
-    # Calculate Orbit Data
-    for tick in range(int(duration/dt)):
-        x.append(myOrbit1.x)
-        y.append(myOrbit1.y)
-        myOrbit1.StepOrbit()
 
-    plt.figure(figsize=(9,9))
-    plt.scatter(x, y, s = 0.1)
-    plt.scatter(0,0,color = "gold", s = 500)
+def plotOrbit(xlim = (-1, 1), ylim = (-1, 1), showPlot = True, savePlot = False, saveName = "orbitPlot.png"):
+    fig, ax = plt.subplots(1,1, figsize = (9, 9))
+    ax.scatter(x, y, s = 0.2, lw=0, marker='.')
+    # Draw Sun
+    ax.scatter(0,0, color = "gold", s = 500, marker='.')
     # Plot will resize such that the orbit will always seem circular if my figure width and height are equal
     # To prevent this, simply anchor the plot size to a set value
-    plt.xlim(-1, 1)
-    plt.ylim(-1, 1)
-    plt.show()
+    ax.set_xlim(xlim[0], xlim[1])
+    ax.set_ylim(ylim[0], ylim[1])
+    if showPlot:
+        plt.show()
+    if savePlot:
+        plt.savefig(saveName)
 
-plotOrbit()
+############################################################
 
+# Initiating Variables
+duration = 20.0 # years
+dt = 0.0001
+e = 0.206
+x0 = 0.39*(1+e)
+# Initiate Orbiting planet
+earth = orbit.Body(0.001, 3.00273e-6)
+mercury = orbit.Body(0.0001, 1.65913e-7)
+startPos = (x0, 0) # In AU
+v0y_Circ = VelYforCircOrbit(np.sqrt(startPos[0]**2 + startPos[1]**2))
+v0y = VelYGeneral(e, x0, earth.mass)
+startVel = (0, v0y) # AU/yr
+B = 2.1
+# Create my Orbit
+myOrbit1 = orbit.Orbit(mercury, startPos, startVel, dt, B)
+# Initiate Data Collection Variables
+x = []
+y = []
+# Calculate Orbit Data
+for tick in range(int(duration/dt)):
+    x.append(myOrbit1.x)
+    y.append(myOrbit1.y)
+    myOrbit1.StepOrbit()
 
-
-
-
-
-
-
-
-# ANIMATION
-
-def Render():
-    pygame.init()
-    # font stuff
-    pygame.font.init()
-    my_font = pygame.font.SysFont('Arial', 30)
-    text_surface = my_font.render('Some Text', False, (100, 100, 100))
-    # init window for display - THIS IS OUR SURFACE
-    displaySurface = pygame.display.set_mode((render.SCREEN_WIDTH, render.SCREEN_HEIGHT))
-    
-    # Clock object to track time
-    clock = pygame.time.Clock()
-    # Orbit
-    t = 0
-    # Initiating Variables
-    dt = 0.0001
-    e = 0.206
-    x0 = 0.39*(1+e)
-    # Initiate Orbiting planet
-    earth = orbit.Body(0.001, 3.00273e-6)
-    startPos = (x0, 0) # In AU
-    v0y_Circ = VelYforCircOrbit(np.sqrt(startPos[0]**2 + startPos[1]**2))
-    v0y = VelYGeneral(e, x0, earth.mass)
-    startVel = (0, v0y_Circ) # AU/yr
-    B = 2.5
-    # Initiate Orbit
-    myOrbit1 = orbit.Orbit(earth, startPos, startVel, dt, B)
-
-    # REndering properties
-    sunDims = (100, 100)
-    planetDims = (50, 50)
-    posCache = [render.WorldToScrCoordsNeg(startPos[0], startPos[1])]
-    sunFile = Path(__file__).parent / Path("planetSprites/Sun.png")
-    SUN_IMAGE = pygame.image.load(sunFile.resolve()).convert_alpha()
-    SUN_IMAGE = pygame.transform.scale(SUN_IMAGE, sunDims)
-    planetFile = Path(__file__).parent / Path("planetSprites/Earth.png")
-    PLANET_IMAGE = pygame.image.load(planetFile.resolve()).convert_alpha()
-    PLANET_IMAGE = pygame.transform.scale(PLANET_IMAGE, planetDims)
-    # run Loop
-    iters = 0
-    while True:
-        displaySurface.fill("black")
-        text_surface = my_font.render('t = {0:.3f} years'.format(t), False, (100, 100, 100))
-        displaySurface.blit(text_surface, (50,0))
-        # player input processing
-        for event in pygame.event.get():
-            if event.type == pygame.QUIT:
-                pygame.quit()
-                raise SystemExit
-            # Sun
-        x, y = render.WorldToScrCoordsNeg(0,0)
-        displaySurface.blit(SUN_IMAGE,(x- sunDims[0]/2, y- sunDims[1]/2))
-        # planet
-        myOrbit1.StepOrbit()
-        x, y = render.WorldToScrCoordsNeg(myOrbit1.x, myOrbit1.y)
-        posCache.append((x, y))
-        displaySurface.blit(PLANET_IMAGE,(x - planetDims[0]/2, y - planetDims[1]/2))
-        # draw trail
-        pygame.draw.lines(displaySurface, (255,255,255), False, posCache)
-        # if (iters > 1/dt): 
-        #     posCache = [(x, y)]
-        #     iters = 0
-        t+=dt
-        iters+=1
-
-        pygame.display.flip() # buffer swapping
-
-Render()
+plotOrbit(showPlot=True)
+myOrbit2 = orbit.Orbit(mercury, startPos, startVel, dt, B)
+render.Animate(myOrbit2, dt = dt, skips = 30)
--- OrbitalMechanics/render.py
+++ OrbitalMechanics/render.py
@@ -1,50 +1,111 @@
 import pygame
 import numpy as np
-
+from pathlib import Path
+import orbit
 SCREEN_WIDTH = 1000
 SCREEN_HEIGHT = 1000
-WORLD_WIDTH = 2
-WORLD_HEIGHT = 2
 
-def WorldToScrCoords(worldX, worldY):
-    scrX = SCREEN_WIDTH/WORLD_WIDTH * worldX
-    scrY = SCREEN_HEIGHT/WORLD_HEIGHT * worldY
+def WorldToScrCoords(worldX, worldY, worldWidth, worldHeight):
+    scrX = SCREEN_WIDTH/worldWidth * worldX
+    scrY = SCREEN_HEIGHT/worldHeight * worldY
     return (scrX, scrY)
 # span -worldx/2 to +worldx/2
-def WorldToScrCoordsNeg(worldX, worldY):
+def WorldToScrCoordsNeg(worldX, worldY, worldWidth, worldHeight):
     # convert world coordinates to one where origin is center
-    transWorldX = worldX + WORLD_WIDTH/2
-    transWorldY = -1*worldY + WORLD_HEIGHT/2
-    scrX, scrY = WorldToScrCoords(transWorldX, transWorldY)
+    transWorldX = worldX + worldWidth/2
+    transWorldY = -1*worldY + worldHeight/2
+    scrX, scrY = WorldToScrCoords(transWorldX, transWorldY, worldWidth, worldHeight)
     return (scrX, scrY)
-
-
-def Init():
-    # initialize pygame
-    pygame.init()
     
+def loadImage(relPath:str, scale = (1, 1)):
+    file = Path(__file__).parent / Path(relPath)
+    image = pygame.image.load(file.resolve()).convert_alpha()
+    image = pygame.transform.scale(image, scale)
+    return image
 
-def run(Execute):
-    # init window for display - THIS IS OUR SURFACE
-    displaySurface = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
-    # Clock object to track time
+# ANIMATION
+# pass in the Orbit Object to Render which will animate the Object Created for the plots!
+def Animate(orbitObj:orbit.Orbit, dt = 0.0001, skips = 1, worldWidth = 2, worldHeight = 2):
+    pygame.init()
     clock = pygame.time.Clock()
+    # font stuff
+    pygame.font.init()
+    my_font = pygame.font.SysFont('Arial', 30)
+    # init window for display - THIS IS OUR SURFACE
+    displaySurface = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT), vsync=1)
+    # REndering properties
+    sunDims = ((SCREEN_WIDTH/worldWidth)/7, (SCREEN_WIDTH/worldWidth)/7)
+    planetDims = (sunDims[0]/4, sunDims[1]/4)
+    posCache = [WorldToScrCoordsNeg(orbitObj.x, orbitObj.y,worldWidth, worldHeight)]
+    # Image Loading
+    SUN_IMAGE = loadImage("planetSprites/Sun.png", sunDims)
+    PLANET_IMAGE = loadImage("planetSprites/Earth.png", planetDims)
     # run Loop
-    dt = 1/60
     t = 0
+    ogSkip = skips
     while True:
         displaySurface.fill("black")
-        # player input processing
+        # Update Text Surface and display it
+        stats_text = 'FPS = {0:.3f}'.format(clock.get_fps())
+        worldDim_text = "World Scaling = {} AU".format(worldWidth)
+        time_surface = my_font.render('t = {0:.3f} years'.format(t), False, (100, 100, 100))
+        speed_surface = my_font.render('speed = {}'.format(skips), False, (100, 100, 100))
+        worldDim_surface = my_font.render(worldDim_text, False, (100, 100, 100))
+        stats_surface = my_font.render(stats_text, False, (100, 100, 100))
+        displaySurface.blits([(time_surface, (20,0)), (stats_surface, (SCREEN_WIDTH - 200,0))])
+        displaySurface.blit(speed_surface, (300, 0))
+        displaySurface.blit(worldDim_surface, (SCREEN_WIDTH/2 - 110, SCREEN_HEIGHT - 50))
+        # User input processing - all events go here
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 pygame.quit()
                 raise SystemExit
-        
-        Execute()
-
+            if event.type == pygame.KEYDOWN:
+                keys = pygame.key.get_pressed()  
+                if keys[pygame.K_UP] or keys[pygame.K_DOWN]:
+                    if keys[pygame.K_UP]:
+                        worldHeight +=1
+                        worldWidth += 1
+                    if keys[pygame.K_DOWN]:
+                        worldHeight -= 1
+                        worldWidth -= 1
+                        if (worldWidth == 0):
+                            worldHeight = 1
+                            worldWidth  = 1
+                    # Update planet and sun sizes as well as clear trail position cache 
+                    # since world dimensions have been changed
+                    sunDims = ((SCREEN_WIDTH/worldWidth)/7, (SCREEN_WIDTH/worldWidth)/7)
+                    planetDims = (sunDims[0]/4, sunDims[1]/4)
+                    SUN_IMAGE = loadImage("planetSprites/Sun.png", sunDims)
+                    PLANET_IMAGE = loadImage("planetSprites/Earth.png", planetDims)
+                    posCache.clear()
+                if event.key == pygame.K_SPACE:
+                    if skips == 0: skips = ogSkip
+                    else:skips = 0 # Pause
+        keys = pygame.key.get_pressed()
+        if keys[pygame.K_RIGHT]:
+            skips += 1
+            ogSkip = skips
+        if keys[pygame.K_LEFT]:
+            skips -= 1
+            if skips < 1: skips = 1
+            ogSkip = skips 
+        # Step The Orbit
+        for i in range(skips):
+            orbitObj.StepOrbit()
+            t+=dt
+        ### RENDERING ###
+        # Sun
+        x, y = WorldToScrCoordsNeg(0,0, worldWidth, worldHeight)
+        displaySurface.blit(SUN_IMAGE,(x- sunDims[0]/2, y- sunDims[1]/2))
+        # Planet
+        x, y = WorldToScrCoordsNeg(orbitObj.x, orbitObj.y, worldWidth, worldHeight)
+        if (len(posCache) == 0):
+            posCache.append((x, y)) # add another of the same point so no error occurs
+        posCache.append((x, y))
+        displaySurface.blit(PLANET_IMAGE,(x - planetDims[0]/2, y - planetDims[1]/2))
+        # Planet Trail
+        pygame.draw.lines(displaySurface, (100,100,100), False, posCache, 1)
         pygame.display.flip() # buffer swapping
-        # update the clock, argument is the frame rate.
-        # computes how many milliseconds passed since previous call
-        # provided argument will delay the loop to keep it within 60 frames per second
-        clock.tick(60) # wait until next frame at 60 fps
-        t += 1
+        # delay 
+        clock.tick(60)
