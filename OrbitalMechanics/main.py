bbfc69c2b629bb5b76d6d1000716dc606104918d
Matias Chavez <mcha7674@outlook.com>
2023-02-28 07:42:08 -0600

    Implemented Precession Angle calculations

--- OrbitalMechanics/main.py
+++ OrbitalMechanics/main.py
@@ -4,14 +4,19 @@ import orbit
 import render
 
 ############### Sub Routines ##################
-def VelYforCircOrbit(distFromSun):
-    return 2 * np.pi * np.sqrt(orbit.Orbit.M / distFromSun)
 def VelYGeneral(e, distFromSun, planetMass):
     return np.sqrt(orbit.Orbit.G*orbit.Orbit.M) * np.sqrt((1-e)/(distFromSun*(1+e))*(1+planetMass/orbit.Orbit.M))
 
-def plotOrbit(xlim = (-1, 1), ylim = (-1, 1), showPlot = True, savePlot = False, saveName = "orbitPlot.png"):
+def CalcPrecessAngle(x, r):
+    return np.arccos(x / r) * (180/np.pi) # Convert angle from radians to degrees with 180/pi
+
+def plotOrbit(data:dict, xlim = (-1, 1), ylim = (-1, 1), showPlot = True, savePlot = False, saveName = "orbitPlot.png", showAphelionPoints = True):
     fig, ax = plt.subplots(1,1, figsize = (9, 9))
-    ax.scatter(x, y, s = 0.2, lw=0, marker='.')
+    ax.scatter(data["x"], data["y"], s = 0.2, lw=0, marker='.')
+    # Aphelion points
+    if showAphelionPoints:
+        ax.scatter(data["xAphelion"], data["yAphelion"], s = 50, marker = '.', label = "Aphelion Point(s)")
+        ax.legend()
     # Draw Sun
     ax.scatter(0,0, color = "gold", s = 500, marker='.')
     # Plot will resize such that the orbit will always seem circular if my figure width and height are equal
@@ -23,32 +28,75 @@ def plotOrbit(xlim = (-1, 1), ylim = (-1, 1), showPlot = True, savePlot = False,
     if savePlot:
         plt.savefig(saveName)
 
+def plotPrecessionAngleVsTime(data:dict, showPlot = True, savePlot = False, saveName = "AngleVsTime.png"):
+    fig, ax = plt.subplots(1,1, figsize = (12, 9))
+    ax.scatter(data["periodPoints"], data["precessAngle"], s = 100, lw=0, marker='.', color = "red")
+    ax.set_title("Precession angle Over time ( Just the first 180 degrees )")
+    ax.set_xlabel("Time (years)")
+    ax.set_ylabel("Precession Angle (Degrees From Start Position)")
+    # Line Fit
+    dTheta, b = np.polyfit(data["periodPoints"], data["precessAngle"], deg=1)
+    data["precessAngleRate"] = dTheta
+    ax.plot(data["periodPoints"], dTheta*np.array(data["periodPoints"]) + b, c="green", linestyle="--", label ="({0:.3f})x + {0:.3f}".format(dTheta, b))
+    ax.legend()
+    if showPlot:
+        plt.show()
+    if savePlot:
+        plt.savefig(saveName)
 ############################################################
 
-# Initiating Variables
+# Initiating time variables
 duration = 20.0 # years
 dt = 0.0001
+# Starting parameters
+# earth starting parameters
+earth = orbit.Body(0.001, 3.00273e-6)
+e = 0
+x0 = 1
+v0y_Circ = VelYGeneral(e, x0, earth.mass)
+# mercury starting parameters
+mercury = orbit.Body(0.0001, 1.65913e-7)
 e = 0.206
 x0 = 0.39*(1+e)
-# Initiate Orbiting planet
-earth = orbit.Body(0.001, 3.00273e-6)
-mercury = orbit.Body(0.0001, 1.65913e-7)
-startPos = (x0, 0) # In AU
-v0y_Circ = VelYforCircOrbit(np.sqrt(startPos[0]**2 + startPos[1]**2))
 v0y = VelYGeneral(e, x0, earth.mass)
 startVel = (0, v0y) # AU/yr
-B = 2.1
-# Create my Orbit
-myOrbit1 = orbit.Orbit(mercury, startPos, startVel, dt, B)
-# Initiate Data Collection Variables
-x = []
-y = []
-# Calculate Orbit Data
-for tick in range(int(duration/dt)):
-    x.append(myOrbit1.x)
-    y.append(myOrbit1.y)
-    myOrbit1.StepOrbit()
+startPos = (x0, 0) # In AU
 
-plotOrbit(showPlot=True)
-myOrbit2 = orbit.Orbit(mercury, startPos, startVel, dt, B)
-render.Animate(myOrbit2, dt = dt, skips = 30)
+# Physics Modifiers
+B = 2.0 # Beta exponent in the Force equation
+a = 0.001 # relativistic alpha term
+
+# Create Orbit instance for Mercury
+myOrbit1 = orbit.Orbit(mercury, startPos, startVel, dt, B, a)
+# Orbit Instance for Earth
+myOrbit2 = orbit.Orbit(earth, startPos, startVel, dt, B, a)
+
+##### Orbit data calculations #####
+data = {"x":[], "y":[], "xAphelion":[], "yAphelion":[], "precessAngle":[0], "periodPoints":[0], "t":[], "precessAngleRate":0}
+# Calculate and store Orbit Data into "data" dictionary
+t = 0
+for tick in range(int(duration/dt)):
+    data["t"].append(t)
+    data["x"].append(myOrbit1.x)
+    data["y"].append(myOrbit1.y)
+    # Get Data for Aphelion Points. 
+    if myOrbit1.isAtAphelion(): 
+        if not myOrbit1.finishedPeriod:
+            data["xAphelion"].append(myOrbit1.x)
+            data["yAphelion"].append(myOrbit1.y)
+            precessionAngle = CalcPrecessAngle(myOrbit1.x, myOrbit1.r)
+            # only store precession angles for the first 180 degrees
+            if (precessionAngle - data["precessAngle"][len(data["precessAngle"]) - 1] >= 0):
+                data["precessAngle"].append( CalcPrecessAngle(myOrbit1.x, myOrbit1.r) )
+                data["periodPoints"].append( t )
+            myOrbit1.finishedPeriod = True
+    if myOrbit1.isAtPerihelion():
+        myOrbit1.finishedPeriod = False
+    myOrbit1.StepOrbit()
+    t+=dt
+
+plotOrbit(data, showPlot=True, savePlot = False, showAphelionPoints = True)
+plotPrecessionAngleVsTime(data)
+print("Rate of angle precession is: ", data["precessAngleRate"], "degrees per year" )
+
+render.Animate(myOrbit1, dt = dt, skips = 30)
--- OrbitalMechanics/orbit.py
+++ OrbitalMechanics/orbit.py
@@ -9,11 +9,13 @@ class Body:
 class Orbit:
     # static class members
     G = 39.478 # AU^3 yr^-2 Ms^-1
-    #a = 1.1e-8 # alpha factor
-    a = 0
     M = 1 # solar mass unit
     # Constructor
-    def __init__(self, body:Body, initPos:tuple, initV:tuple, dt:float, B=2.0):
+    def __init__(self, body:Body, initPos:tuple, initV:tuple, dt:float, B=2.0, a = 1.1e-8):
+        self.SetDefaultParamaters(body, initPos, initV, dt, B, a)
+        
+    # Initiating function (Set all the parameters)
+    def SetDefaultParamaters(self, body:Body, initPos:tuple, initV:tuple, dt:float, B, a):
         # Planet
         self.body = body
         self.m = body.mass
@@ -26,8 +28,11 @@ class Orbit:
         self.vx = initV[0]
         self.vy = initV[1]
         self.v = np.sqrt(self.vx**2 + self.vy**2)
+        self.v_minus1 = 0
         # Beta Constant for Inverse Law
         self.B = B
+        # Alpha constant for relativistic Factor
+        self.a = a
         # Force Members
         self.fx = (self.G * self.M * self.m) / pow(self.r, self.B + 1.0) \
         * (1+ (self.a/pow(self.r,2))) * self.x
@@ -41,7 +46,10 @@ class Orbit:
         # time 
         self.t = 0
         self.dt = dt
+        # Period
+        self.finishedPeriod = True
     
+
     def StepOrbit(self):
         # Evaluating PDE's
         self.r = np.sqrt(self.x**2 + self.y**2)
@@ -52,6 +60,7 @@ class Orbit:
         * (1+ (self.a/pow(self.r,2))) * self.y
         self.f = np.sqrt(self.fx**2 + self.fy**2)
         # updating velocity
+        self.v_minus1 = self.v # store previous velocity for use in finding period
         self.vx = self.vx - (self.fx / self.body.mass) * self.dt
         self.vy = self.vy - (self.fy / self.body.mass) * self.dt
         self.v = np.sqrt(self.vx**2 + self.vy**2)
@@ -62,6 +71,16 @@ class Orbit:
         self.PE = -1 * (self.G*self.M * self.body.mass) / self.r
         self.KE = 0.5 * self.body.mass * pow(self.v, 2)
         self.E = self.KE + self.PE
+
+    def isAtAphelion(self):
+        vAfter = self.v
+        vBefore = self.v_minus1
+        return ((vAfter - vBefore) > 0)
+        
+    def isAtPerihelion(self):
+        vAfter = self.v
+        vBefore = self.v_minus1
+        return ((vAfter - vBefore) < 0)
 
         
 
